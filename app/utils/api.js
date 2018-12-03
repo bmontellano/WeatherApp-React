02b9b0108d40b8bbf47fdcc08e344b3d4f59dac9
bmontellano <bmontellano@gmail.com>
2018-12-03 09:26:00 -0800

    first commit, first push

---
+++ app/utils/api.js
@@ -0,0 +1,24 @@
+
+
+
+
+/*
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  */
